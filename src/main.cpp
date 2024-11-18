9706c2f30edeb5c443319a823533edb0b01cfa69
FrostMonster (Sharlabr) <frostmonster@yandex.ru>
2024-11-18 16:44:23 +0300

    MainWindow переименован в AppWindow

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <gtkmm.h>
 
-#include "gui/desktop/snake/MainWindow.h"
+#include "gui/desktop/snake/AppWindow.h"
 
 auto main(int argc, char *argv[]) -> int {
   auto app = Gtk::Application::create("org.gtkmm.example");
-  s21::MainWindow window;
-  return app->make_window_and_run<s21::MainWindow>(argc, argv);
+  s21::AppWindow window;
+  return app->make_window_and_run<s21::AppWindow>(argc, argv);
 }
