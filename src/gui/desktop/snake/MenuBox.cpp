70f2664b5f87e5a86b529eeae9740e745cfe4691
FrostMonster (Sharlabr) <frostmonster@yandex.ru>
2024-11-18 16:45:06 +0300

    Заготовка под главное меню

---
+++ src/gui/desktop/snake/MenuBox.cpp
@@ -0,0 +1,4 @@
+//
+// Created by frostmonster on 18.11.2024
+//
+#include "MenuBox.h"
---
+++ src/gui/desktop/snake/MenuBox.h
@@ -0,0 +1,13 @@
+//
+// Created by frostmonster on 18.11.2024
+//
+#ifndef MENUBOX_H
+#define MENUBOX_H
+
+#include "gtkmm/box.h"
+
+namespace s21 {
+class MenuBox : public Gtk::Box {};
+}  // namespace s21
+
+#endif  // MENUBOX_H
