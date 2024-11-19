d528d96f7cd5ce08514822e7e763b06d196f704c
FrostMonster (Sharlabr) <frostmonster@yandex.ru>
2024-11-19 21:02:34 +0300

    Изменение общего параметра kRatio

--- src/headers/constants.h
+++ src/headers/constants.h
@@ -2,12 +2,12 @@
 #define CONSTANTS_H
 
 namespace s21::constants {
-constexpr int kFieldHeight = 20;
-constexpr int kFieldWidth = 10;
-constexpr int kSpacing = 10;
-constexpr double kRatio = 0.5;
-constexpr int kWinHeight = 600;
-constexpr int kWinWidth = 400;
+const int kFieldHeight = 20;
+const int kFieldWidth = 10;
+const int kSpacing = 10;
+const float kRatio = 3. / 4.;
+const int kWinHeight = 800;
+const int kWinWidth = 580;
 }  // namespace s21::constants
 
 #endif  // CONSTANTS_H
