7611a532158a1d075b747f4b5d59cafdc3bc7017
FrostMonster (Sharlabr) <frostmonster@yandex.ru>
2024-11-20 16:32:08 +0300

    Замена id

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,7 @@
 #include "gui/desktop/snake/app_window.h"
 
 auto main(int argc, char *argv[]) -> int {
-  auto app = Gtk::Application::create("org.example.BrickGame2");
+  auto app = Gtk::Application::create("edu.school21.BrickGame2");
   s21::AppWindow window;
   return app->make_window_and_run<s21::AppWindow>(argc, argv);
 }
