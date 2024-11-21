4130e690e8f05cc55f42151235f755f5a435b8b4
FrostMonster (Sharlabr) <frostmonster@yandex.ru>
2024-11-21 17:52:38 +0300

    Реализован контейнер главного меню

--- src/gui/desktop/snake/menu_box.cc
+++ src/gui/desktop/snake/menu_box.cc
@@ -2,3 +2,31 @@
 // Created by frostmonster on 18.11.2024
 //
 #include "menu_box.h"
+
+namespace s21 {
+
+MenuBox::MenuBox() {
+  start_button_.set_label("Start");
+  level_button_.set_label("Level");
+  speed_button_.set_label("Speed");
+  exit_button_.set_label("Exit");
+
+  start_button_.set_margin(5);
+
+  level_button_.set_margin(5);
+  speed_button_.set_margin(5);
+  exit_button_.set_margin(5);
+
+  set_hexpand(true);
+  set_orientation(Gtk::Orientation::VERTICAL);
+  set_valign(Gtk::Align::CENTER);
+  set_halign(Gtk::Align::CENTER);
+  append(start_button_);
+  append(level_button_);
+  append(speed_button_);
+  append(exit_button_);
+}  // MenuBox::MenuBox
+
+MenuBox::~MenuBox() = default;
+
+}  // namespace s21
--- src/gui/desktop/snake/menu_box.h
+++ src/gui/desktop/snake/menu_box.h
@@ -1,13 +1,26 @@
 //
 // Created by frostmonster on 18.11.2024
 //
-#ifndef MENUBOX_H
-#define MENUBOX_H
+#ifndef MENU_BOX_H
+#define MENU_BOX_H
 
-#include "gtkmm/box.h"
+#include <gtkmm/box.h>
+#include <gtkmm/button.h>
 
 namespace s21 {
-class MenuBox : public Gtk::Box {};
+class MenuBox : public Gtk::Box {
+ public:
+  MenuBox();
+  ~MenuBox() override;
+
+ private:
+  Gtk::Button start_button_;
+  Gtk::Button level_button_;
+  Gtk::Button speed_button_;
+  Gtk::Button exit_button_;
+
+  void StartButtonClicked_();
+};  // class MenuBox
 }  // namespace s21
 
-#endif  // MENUBOX_H
+#endif  // MENU_BOX_H
