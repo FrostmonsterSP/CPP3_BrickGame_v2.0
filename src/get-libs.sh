a8b0dbbdbae3251df3b3f21c401bc331a7ccd7d4
Sharla Bryanna <sharlabr@student.21-school.ru>
2024-11-23 19:56:44 +0300

    Переработка сборки для совместимости с зависимостями

--- src/get-libs.sh
+++ src/get-libs.sh
@@ -10,6 +10,8 @@ BOLD="\033[1m"
 RESET="\033[0m"
 
 LIB_VERSION=2.2
+LIB_DEST=$2
+INC_DEST=$1
 
 if [ ! -d "libs" ]; then mkdir libs; fi
 
@@ -17,8 +19,9 @@ if [ $(uname -m) == "x86_64" ]; then
 printf "$BLUE$BOLD=>$RESET$BLUE Скачивается скомпилированная x86_64 библиотека$RESET\n"
 wget https://github.com/FrostmonsterSP/BrickGameLib/releases/download/v.$LIB_VERSION/brick_game_lib-$LIB_VERSION.x86_64.tar.gz
 tar -xf brick_game_lib-$LIB_VERSION.x86_64.tar.gz
-rm -rf brick_game_lib-$LIB_VERSION.x86_64.tar.gz
-mv engine.h libs/tetris.h
+mv libs/* $LIB_DEST
+rm -rf brick_game_lib-$LIB_VERSION.x86_64.tar.gz libs
+mv engine.h $INC_DEST/tetris.h
 else
 printf "$BLUE$BOLD=>$RESET$BLUE Скачиваются исходные коды библиотеки$RESET\n"
 wget https://github.com/FrostmonsterSP/BrickGameLib/archive/refs/tags/v.$LIB_VERSION.tar.gz
@@ -26,8 +29,8 @@ tar -xf v.$LIB_VERSION.tar.gz
 rm -rf v.$LIB_VERSION.tar.gz
 cd BrickGameLib-v.$LIB_VERSION
 make build
-mv libs/libtetris.a ../libs/libtetris.a
-mv src/engine.h ../libs/tetris.h
+mv libs/libtetris.* ../$LIB_DEST/
+mv src/engine.h ../$2/tetris.h
 cd ../
 rm -rf BrickGameLib-v.$LIB_VERSION
 fi
