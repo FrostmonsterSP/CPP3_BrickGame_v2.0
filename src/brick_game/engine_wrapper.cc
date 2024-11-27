75ea8c9fb90982f6464a8421b6af7559ebee627d
FrostMonster (Sharlabr) <frostmonster@yandex.ru>
2024-11-27 16:09:07 +0300

    Класс-обёртка EngineWrapper завершён (вроде)

--- src/brick_game/engine_wrapper.cc
+++ src/brick_game/engine_wrapper.cc
@@ -1,45 +1,67 @@
 #include "engine_wrapper.h"
 
-#include "engine_defs.h"
-
 namespace s21 {
+
 namespace engine {
 extern "C" {
 #include "engine.h"
 }
 }  // namespace engine
 
-EngineWrapper::EngineWrapper() { UpdateGameState(); }
-
-void EngineWrapper::UserInput(engine::UserAction_t action) {
-  userInput(action);
-}
+EngineWrapper::EngineWrapper()
+    : kFieldHeight(FIELD_HEIGHT),
+      kFieldWidth(FIELD_WIDTH),
+      kNextHeight(FIGURE_HEIGHT),
+      kNextWidth(FIGURE_WIDTH),
+      m_game_info_(engine::updateCurrentState()) {}
 
 auto EngineWrapper::UpdateGameState() -> void {
-  game_info_ = engine::updateCurrentState();
-  if (game_info_ == nullptr) {
+  m_game_info_ = engine::updateCurrentState();
+  if (m_game_info_ == nullptr) {
     throw std::runtime_error("UpdateGameState failed");
   }
 }
 
 auto EngineWrapper::GetFieldCell(int row, int col) -> int {
-  return game_info_->field[row][col];
-}
+  return m_game_info_->field[row][col];
+}  // EngineWrapper::GetFieldCell(int row, int col)
 
 auto EngineWrapper::GetNextCell(int row, int col) -> int {
-  return game_info_->next[row][col];
-}
+  return m_game_info_->next[row][col];
+}  // EngineWrapper::GetNextCell(int row, int col)
 
-auto EngineWrapper::GetScore() -> int { return game_info_->score; }
+auto EngineWrapper::GetScore() -> int { return m_game_info_->score; }
 
-auto EngineWrapper::GetHighScore() -> int { return game_info_->high_score; }
+auto EngineWrapper::GetHighScore() -> int { return m_game_info_->high_score; }
 
-auto EngineWrapper::GetLevel() -> int { return game_info_->level; }
+auto EngineWrapper::GetLevel() -> int { return m_game_info_->level; }
 
-auto EngineWrapper::GetSpeed() -> int { return game_info_->speed; }
+auto EngineWrapper::GetSpeed() -> int { return m_game_info_->speed; }
 
-auto EngineWrapper::GetState() -> engine::GameState_t {
-  return game_info_->state;
-}
+auto EngineWrapper::GetState() -> int { return m_game_info_->state; }
+
+auto EngineWrapper::IsIdle() -> bool {
+  return m_game_info_->state == engine::GameState_t::StateIdle;
+}  // EngineWrapper::IsIdle()
+
+void UserActions::Terminate() {
+  engine::userInput(engine::UserAction_t::Terminate);
+}  // UserActions::Terminate()
+
+void UserActions::Start() { engine::userInput(engine::UserAction_t::Start); }
+
+void UserActions::Pause() { engine::userInput(engine::UserAction_t::Pause); }
+
+void UserActions::Resume() { engine::userInput(engine::UserAction_t::Start); }
+
+void UserActions::Left() { engine::userInput(engine::UserAction_t::Left); }
+
+void UserActions::Right() { engine::userInput(engine::UserAction_t::Right); }
+
+void UserActions::Up() { engine::userInput(engine::UserAction_t::Up); }
+
+void UserActions::Down() { engine::userInput(engine::UserAction_t::Down); }
+
+void UserActions::Action() { engine::userInput(engine::UserAction_t::Action); }
 
 }  // namespace s21
--- src/brick_game/engine_wrapper.h
+++ src/brick_game/engine_wrapper.h
@@ -6,31 +6,47 @@
 namespace s21 {
 
 namespace engine {
-extern "C" {
 #include "engine_defs.h"
-}
 }  // namespace engine
+
+struct UserActions {
+ public:
+  UserActions() = default;
+  ~UserActions() = default;
+  void Terminate();
+  void Start();
+  void Pause();
+  void Resume();
+  void Left();
+  void Right();
+  void Up();
+  void Down();
+  void Action();
+};  // struct UserActions
 
 class EngineWrapper {
  public:
   EngineWrapper();
   ~EngineWrapper() = default;
 
-  static auto GetFieldHeight() -> int { return FIELD_HEIGHT; }
-  static auto GetFieldWidth() -> int { return FIELD_WIDTH; }
-  void UserInput(engine::UserAction_t);
+  const int kFieldHeight;
+  const int kFieldWidth;
+  const int kNextHeight;
+  const int kNextWidth;
+  UserActions Action;
   void UpdateGameState();
-
   auto GetFieldCell(int, int) -> int;
   auto GetNextCell(int, int) -> int;
   auto GetScore() -> int;
   auto GetHighScore() -> int;
   auto GetLevel() -> int;
   auto GetSpeed() -> int;
-  auto GetState() -> engine::GameState_t;
+  auto GetState() -> int;
+  auto IsStarted() -> bool;
+  auto IsIdle() -> bool;
 
  private:
-  const engine::GameInfo_t *game_info_;
+  const engine::GameInfo_t *m_game_info_;
 };
 
 }  // namespace s21
