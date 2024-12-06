a0c97107effbeb1f31c1f1de5e658eccefee68df
FrostMonster (Sharlabr) <frostmonster@yandex.ru>
2024-12-06 12:17:08 +0300

    Переписана боковая панель

--- src/gui/desktop/snake/side_panel.cc
+++ src/gui/desktop/snake/side_panel.cc
@@ -3,31 +3,25 @@
 //
 #include "side_panel.h"
 
+#include "engine_defs.h"
+
 namespace s21 {
-SidePanel::SidePanel(EngineWrapper* engine)
-    : m_next_frame_(4, 4), m_engine_(engine) {
-  m_next_frame_.SetEngine(m_engine_);
+SidePanel::SidePanel(EngineWrapper& engine)
+    : m_engine_(engine),
+      m_next_frame_({FIGURE_WIDTH, FIGURE_HEIGHT}, 1, engine) {
   m_next_frame_.add_css_class("main-field");
   set_orientation(Gtk::Orientation::VERTICAL);
-
-  auto score = m_engine_->GetScore();
-  auto high_score = m_engine_->GetHighScore();
-  auto level = m_engine_->GetLevel();
-  auto speed = m_engine_->GetSpeed();
 
   m_score_label_.set_halign(Gtk::Align::CENTER);
   m_high_score_label_.set_halign(Gtk::Align::CENTER);
   m_level_label_.set_halign(Gtk::Align::CENTER);
   m_speed_label_.set_halign(Gtk::Align::CENTER);
 
-  m_score_label_.set_text("Score: " + std::to_string(score));
-  m_high_score_label_.set_text("High score: " + std::to_string(high_score));
-  m_level_label_.set_text("Level: " + std::to_string(level));
-  m_speed_label_.set_text("Speed: " + std::to_string(speed));
-
   m_next_frame_.set_vexpand(false);
   set_hexpand(false);
   set_margin(kSpacing * 2);
+
+  DrawPanel();
 
   append(m_next_frame_);
 
@@ -38,4 +32,17 @@ SidePanel::SidePanel(EngineWrapper* engine)
 }
 
 SidePanel::~SidePanel() = default;
+
+void SidePanel::DrawPanel() {
+  m_score_ = m_engine_.GetScore();
+  m_high_score_ = m_engine_.GetHighScore();
+  m_level_ = m_engine_.GetLevel();
+  m_speed_ = m_engine_.GetSpeed();
+
+  m_score_label_.set_text("Score: " + std::to_string(m_score_));
+  m_high_score_label_.set_text("High score: " + std::to_string(m_high_score_));
+  m_level_label_.set_text("Level: " + std::to_string(m_level_));
+  m_speed_label_.set_text("Speed: " + std::to_string(m_speed_));
+  m_next_frame_.DrawField();
+}
 }  // namespace s21
--- src/gui/desktop/snake/side_panel.h
+++ src/gui/desktop/snake/side_panel.h
@@ -12,18 +12,23 @@
 namespace s21 {
 class SidePanel : public Gtk::Box {
  public:
-  SidePanel() = delete;
-  SidePanel(EngineWrapper*);
+  SidePanel(EngineWrapper&);
   ~SidePanel() override;
+
+  void DrawPanel();
 
  private:
   const int kSpacing = 10;
+  int m_score_;
+  int m_high_score_;
+  int m_level_;
+  int m_speed_;
   Gtk::Label m_score_label_;
   Gtk::Label m_high_score_label_;
   Gtk::Label m_level_label_;
   Gtk::Label m_speed_label_;
+  EngineWrapper& m_engine_;
   CellsGrid m_next_frame_;
-  EngineWrapper* m_engine_;
 };  // class SidePanel
 }  // namespace s21
 
