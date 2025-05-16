a016e8e51888e896e6623e5609e8caac0b7c7da2
Sharla <sharlabr@21-school.ru>
2025-05-16 11:49:30 +0300

    Namespace в вызове приложения

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,6 @@
 #include "gui/desktop/game_app.h"
 
 auto main(int argc, char* argv[]) -> int {
-  auto application = GameApplication::Create();
+  auto application = s21::GameApplication::Create();
   return application->run(argc, argv);
 }
